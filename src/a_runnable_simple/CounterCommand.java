d989291c27d236a6024d4ae03192abca3dc77e06
Reinhold Buchinger <buc@htl.rennweg.at>
2024-02-11 20:54:45 +0100

    improve first threads example

--- src/a_runnable_simple/CounterCommand.java
+++ src/a_runnable_simple/CounterCommand.java
@@ -5,7 +5,8 @@ package a_runnable_simple;
 public class CounterCommand implements Runnable {
     @Override
     public void run() {
-        for ( int i = 0; i < 20; i++ ) {
+        for ( int i = 0; i < 2000; i++ ) {
+            Math.sqrt(i*34234.6242); // just to waste some time
             System.out.println(i);
         }
     }
--- src/a_runnable_simple/HelloCommand.java
+++ src/a_runnable_simple/HelloCommand.java
@@ -3,7 +3,8 @@ package a_runnable_simple;
 
 public class HelloCommand implements Runnable {
     @Override public void run() {
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 2000; i++) {
+            Math.sqrt(i*934234.6242); // just to waste some time
             System.out.println("Hallo Welt!");
         }
     }
